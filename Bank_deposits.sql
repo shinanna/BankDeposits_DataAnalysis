bd11afdc01c660ac8cdaa76d1787aa8f1c500bdd
shinanna <93386700+shinanna@users.noreply.github.com>
2021-12-17 16:54:34 +0100

    Add files via upload

---
+++ Bank_deposits.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE bank_deposits;
+
+USE bank_deposits;
+
+SELECT * FROM bank_deposits.fulltable;
+
+ALTER TABLE fulltable 
+RENAME COLUMN MyUnknownColumn
+TO id;
+
+CREATE TABLE clients 
+SELECT id, age, job, marital, education 
+FROM fulltable;
+
+CREATE TABLE bk_profile
+SELECT id, balance, housing, loan
+FROM fulltable;
+
+ALTER TABLE fulltable
+RENAME COLUMN `Subscription to deposit (target)`
+TO target_subscription;
+
+CREATE TABLE campaign
+SELECT id, contact, duration, previous, pdays, poutcome, target_subscription
+FROM fulltable;
